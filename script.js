055083feef79b09aa37e09f73c7c10b575f309a1
nanarama <annasampers@gmail.com>
2023-04-18 16:17:03 -0500

    Add player class

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
+const Cell = () => {
+  let value = 0;
+
+  const addToken = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return {addToken, getValue};
+}
+
 const gameboard = (() => {
-  let board = ['x','o','x','o','x','o','x','o','x'];
+  const rows = 3;
+  const columns = 3;
+  const board = [];
   
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+    console.log(boardWithCellValues);
+  };
+
   const display = () => {
     const rows = document.getElementById('gameboard');
     for (const child of rows.children) {
       child.innerHTML = board[child.id]
     };
     };
-  return { display };
+  return {display, printBoard};
 })();
 
+const Player = (name, token) => {
+  const getName = () => name;
+  const getToken = () => token;
+  return {getName, getToken};
+}
+
+const GameController = () => {
+  const board = gameboard;
+  const players = [Player('anna','X'), Player('andrew', 'O')];
+  return {players}
+}
+
+
 gameboard.display()
+gameboard.printBoard()
+const game = GameController()
